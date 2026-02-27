d3e22aea95896abb6fd415f5d81b68e106d510b0
UGR/4010/16 <yoaszerga@gmail.com>
2026-02-27 07:27:32 -0800

    Implement numRescueBoats function to save people

---
+++ boats-to-save-people.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        result = 0
+        left = 0
+        right= len(people)-1
+        while left <= right:
+            if people[left]+people[right]<=limit:
+                left+=1
+                right-=1
+            else:
+                right-=1
+            result +=1
+
+        return result

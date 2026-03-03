5fb0380285a60ddbb1d2aac7afb5d000ce09bbc0
UGR/4010/16 <yoaszerga@gmail.com>
2026-03-03 08:27:23 -0800

    Implement subarray sum equals k solution

---
+++ subarray-sum-equals-k.py
@@ -0,0 +1,14 @@
+from typing import List
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        count = 0
+        prefix_sum = 0
+        prefix_count = {0: 1}
+        
+        for num in nums:
+            prefix_sum += num
+            if prefix_sum - k in prefix_count:
+                count += prefix_count[prefix_sum - k]
+            prefix_count[prefix_sum] = prefix_count.get(prefix_sum, 0) + 1
+        
+        return count

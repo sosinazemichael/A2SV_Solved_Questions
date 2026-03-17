4e46f64ee6f479a9a41359bcbfa1bd9874219d9e
UGR/4010/16 <yoaszerga@gmail.com>
2026-03-17 12:00:26 -0800

    Create binary-subarrays-with-sum.py

---
+++ binary-subarrays-with-sum.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        # The exact sum is the difference between at most 'goal' 
+        # and at most 'goal - 1'
+        return self.at_most(nums, goal) - self.at_most(nums, goal - 1)
+
+    def at_most(self, nums: List[int], goal: int) -> int:
+        if goal < 0:
+            return 0
+            
+        tail = 0
+        current_sum = 0
+        count = 0
+        
+        for head in range(len(nums)):
+            current_sum += nums[head]
+            
+            while current_sum > goal and tail <= head:
+                current_sum -= nums[tail]
+                tail += 1
+            
+            count += (head - tail + 1)
+            
+        return count

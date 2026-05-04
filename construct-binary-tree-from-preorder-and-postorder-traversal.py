afd018b503c15e50cd8588e2dc1332957f76d660
UGR/4010/16 <yoaszerga@gmail.com>
2026-05-04 07:09:15 -0800

    Implement binary tree construction from preorder and postorder

---
+++ construct-binary-tree-from-preorder-and-postorder-traversal.py
@@ -0,0 +1,24 @@
+class Solution:
+    def constructFromPrePost(self, preorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        if not preorder or not postorder:
+            return None
+        
+        root = TreeNode(preorder[0])
+        
+        if len(preorder) == 1:
+            return root
+        
+        left_root_val = preorder[1]
+        left_size = postorder.index(left_root_val) + 1
+        
+        root.left = self.constructFromPrePost(
+            preorder[1 : left_size + 1], 
+            postorder[: left_size]
+        )
+        
+        root.right = self.constructFromPrePost(
+            preorder[left_size + 1 :], 
+            postorder[left_size : -1]
+        )
+        
+        return root
